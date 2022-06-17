ff2f22b258c15f31938f091861b095d382de948f
Aniket bansal <77097101+Aniket-bansal@users.noreply.github.com>
2022-06-17 13:04:30 +0530

    order

---
+++ src/NavbarComponents/OrderPlacedComponent/Order.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './order.css'
+const Order = () => {
+  return (
+    <div>
+        <img src="https://cdn.dribbble.com/users/2572904/screenshots/17169793/media/ed801ffe0fbeb4b95ca246ba1f5ea398.gif" />
+        <h2>Thank You!!</h2>
+        
+        <h2>Your Order has been Placed</h2>
+        <h2>Your Order Will Reach You Soon !!!</h2>
+
+    </div>
+  )
+}
+
+export default Order
