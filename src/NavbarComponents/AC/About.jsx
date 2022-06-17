c8748de0ff2051e283601d8fde76c76f020c8edb
Aniket bansal <77097101+Aniket-bansal@users.noreply.github.com>
2022-06-17 13:06:37 +0530

    about/contact

---
+++ src/NavbarComponents/AC/About.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+import '../style/About.css'
+const About = () => {
+  return (
+    <div>
+        <img className="top-banner" src="https://cdn.shopify.com/s/files/1/0361/8553/8692/files/green_desk_2.jpg?v=1650968222" alt="Not Found!" />
+        <br/>
+        <p>We want to change how we look after ourselves by drawing attention to the areas and the problems that we often overlook, or consider unimportant - but do in fact, affect how we feel and consequently how we appear. By addressing the root of these issues, we bring to you the key to a longer-lasting, deeper and more meaningful version of beautiful.</p>
+        <br/>
+        <br/>
+        <div className="data">
+            <div className="container1">
+                <div><img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/1.jpg?v=108853718973995015521655208137" alt="Not Found"/></div>
+                <div>
+                <h3>BRINGING THE ‘CARE’ <br/>BACK TO SKINCARE</h3>
+                <p>Our nature-forward approach to formulation ensures our ingredients and actives are in their most natural skin-friendly usable form. We take every precaution to eliminate anything that has the smallest chance to adversely affect your body and our environment. Every one of our products are tested clinically to ensure 100% dermatological safety.</p>
+                </div>
+            </div >
+            <div className="container2">
+                <div><h3>WE WON’T COMPROMISE,<br/>AND NEITHER SHOULD YOU</h3>
+                <p>When we say we develop highly targeted remedial products, we mean it. From the ingredients, to the textural experience, we’re making sure that it’s all closely monitored, formulated and repeatedly tested. We simply won’t launch a product if it does not live up to a very high standard of quality and efficacy.</p>
+                </div>
+                <div>
+                    <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/2.jpg?v=27803939483609228111655208138" alt="Not Found" />
+                </div>
+            </div>
+        </div>
+        <div className="dataLink">
+         <h3>WORKING TOWARDS A <br/>BEAUTIFUL TOMORROW</h3>
+         <p>We do not use ingredients which will hinder the longevity of our natural resources, or harm our furry friends - be it their habitats or the animals themselves. We’ve also started a recycling programme for our containers. It’s the little things that matter, and every little effort can count positively towards saving our planet.</p>
+         <a href="https://www.dotandkey.com/pages/csr">Find out more about our Earth Program here > </a>
+        </div>
+        <div className="ifra">
+            <div><img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/3.jpg?v=143273889409969834581655208138" alt="Not Found"/></div>
+            <div>
+                <h3>WE KNOW WHEN TO SAY NO</h3>
+                <p>Our products are completely free of nasty, and we only use IFRA certified fragrances and essential oils. We say NO to all the following ingredients :</p><br/><br/><br/><br/><br/><br/><br/><br/>
+                <h4>- Parabens (methyl, propyl, etc.)</h4>
+                <h4>- Sulphates (SLS, SLES)</h4>
+                <h4>- Alcohol (Ethanol)</h4>
+                <h4>- PABA</h4>
+                <h4>- Phthalates</h4>
+                <h4>- Triclosan</h4>
+                <h4>- Formaldehyde Donors</h4>
+                <h4>- Heavy metals</h4>
+                <h4>- Synthetic Dyes</h4>
+            </div>
+        </div>
+        <div className="promises">
+            <h3>THE DOT & KEY PROMISE</h3>
+            <div className="item">
+                <div>
+                        <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/crnf.jpg?v=144518637420478652841655208154" alt="Not Found!" />
+                        <div>
+                            <h4>CONSUMER RESEARCH & NEED FULFILMENT</h4>
+                            <p>We always have our ear to the ground listening to consumer needs, and feed that to our team of skilled chemists who then work on new product ideas, new solutions, research on blends, cutting edge technology and creating prototypes.  </p>
+                        </div>
+                </div>
+                <div>
+                <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/Testing-validation.jpg?v=96291345747492281441655208194" alt="Not Found!" />
+                    <div>
+                        <h4>TESTING & VALIDATION</h4>
+                        <p>We’re unaffected by trends because we launch new products only in response to genuine unaddressed needs expressed by our customers. And only after extensive research, do we deliver formulations of the highest quality and safety. All our products released only after a high performance grading standard is met.  </p>
+                    </div>
+                </div>
+                <div>
+                <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/ima.jpg?v=93715680922064519971655208172" alt="Not Found!" />
+                    <div>
+                        <h4>INGREDIENT & MATERIAL ASSESSMENT</h4>
+                        <p>We like to be ahead of the curve, both in the technology we use and the science we incorporate. Our ingredients are sourced from the most reputed suppliers with all the necessary compliances verified and checked. As a result, our products promise to always be innovative, gentle-to-use and have the lowest impact on the environment.</p>
+                    </div>
+                </div>
+                <div>
+                <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/Label-transparency.jpg?v=43075071097887518831655208176g" alt="Not Found!" />
+                    <div>
+                        <h4>LABEL TRANSPARENCY</h4>
+                        <p>We believe you have a right to know what goes into your products. We list our ingredients clearly and consistently according to established standards. Our products are dermatologically tested to be 100% safe on skin, and are safe to life. We carefully formulate each of our products with “The Good Stuff” using as much from nature as possible.   </p>
+                    </div>
+                </div>
+                <div>
+                <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/Production.jpg?v=103117539792042279541655208185" alt="Not Found!" />
+                    <div>
+                        <h4>PRODUCTION</h4>
+                        <p>We choose to work with our supply-chain & manufacturing partners after a thorough study of their quality standards and consistency. We evaluate based on compliance with our environment, technological, sanitisation and ethical standards.   </p>
+                    </div>
+                </div>
+                <div>
+                <img src="https://cdn.shopify.com/s/files/1/0361/8553/8692/t/547/assets/Packaging.jpg?v=134638703072571645821655208181"  alt="Not Found!"/>
+                    <div>
+                        <h4>PACKAGING</h4>
+                        <p>Making everyday beautiful matters to us; we design to inspire little joys. However, our packaging development isn’t just pretty to look at. We are mindful of its composition as well, focusing on inks and materials that are environmentally responsible and eliminating materials of concern. </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
+
+
+
+// modal overlay
+// function BackdropExample() {
+//     const OverlayOne = () => (
+//       <ModalOverlay
+//         bg='blackAlpha.300'
+//         backdropFilter='blur(10px) hue-rotate(90deg)'
+//       />
+//     )
+  
+//     const { isOpen, onOpen, onClose } = useDisclosure()
+//     const [overlay, setOverlay] = React.useState(<OverlayOne />)
+  
+//     return (
+//       <>
+//         <Button
+//           onClick={() => {
+//             setOverlay(<OverlayOne />)
+//             onOpen()
+//           }}
+//         >
+//           Use Overlay one
+//         </Button>
+        
+//         <Modal isCentered isOpen={isOpen} onClose={onClose} >
+//           {overlay}
+//           <ModalContent >
+//             <ModalHeader></ModalHeader>
+//             <ModalCloseButton />
+//             <ModalBody>
+//               <Text>Custom backdrop filters!</Text>
+//             </ModalBody>
+//             <ModalFooter>
+              
+//             </ModalFooter>
+//           </ModalContent>
+//         </Modal>
+//       </>
+//     )
+//   }
